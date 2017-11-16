246f4710f5aa744429f489b0e537c8a7a03f0c91
Bikalpa Pahari <bp359@afsconnect1.njit.edu>
2017-11-16 16:09:47 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<? php
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+
+define('connection','sql2.njit.edu');
+define('username','bp359');
+define('password','bTDLnIOQ');
+define('dbname','bp359');
+
+<?
